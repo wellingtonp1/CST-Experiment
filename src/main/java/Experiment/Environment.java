583c81d68a3df984f95fd386952fcc41cc159279
Wellington Ferreira <40180607+wellingtonp1@users.noreply.github.com>
2022-04-26 22:32:51 -0300

    Delete src directory

--- src/main/java/Experiment/Environment.java
+++
@@ -1,9 +0,0 @@
-package Experiment;
-
-public class Environment {
-	
-	public String datasetpath = "C:\\Users\\wellf\\OneDrive\\UNIFESP\\Dissertaçao\\DB\\Oasis\\oasis_longitudinal.csv";
-	public Environment() {
-		System.out.println("dataset has been loaded successfully");
-	}
-}
